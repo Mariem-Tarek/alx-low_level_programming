dc222db83f635090fcb7d507604e263ef183d8a8
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-08 16:35:54 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+a = 0;
+while (a <= 9)
+	{
+printf("%d", a);
+a++;
+	}
+	putchar('\n');
+	return (0);
+}
