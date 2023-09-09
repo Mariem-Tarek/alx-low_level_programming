22e3bcb170f9b6614a76760f69701a7e63f4b495
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-08 17:04:13 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+a = 'z';
+while (a >= 'z')
+	{
+putchar(a);
+a--;
+	}
+	putchar('\n');
+	return (0);
+}
