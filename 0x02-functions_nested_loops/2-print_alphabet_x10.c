758a7b5f4e1901f9b7bbe3a96e3b6c9c7957a76d
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-12 13:15:17 -0700

    ALX :)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints
+ *
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+int j;
+int i;
+
+for (j = 0; j < 10; j++)
+{
+
+for (i = 0; i < 26; i++)
+{
+	_putchar('a' + i);
+}
+	_putchar('\n');
+}
+}
