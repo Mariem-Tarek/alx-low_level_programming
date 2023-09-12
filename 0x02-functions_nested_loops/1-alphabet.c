c4b83e0beaab05aa6cfff9ec1cc18baa72f08048
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-12 13:43:25 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints
+ *
+ *Description : a function that print the alphapet
+ *
+ *
+ */
+void print_alphabet(void)
+{
+int i;
+
+for (i = 0; i < 26; i++)
+{
+	 _putchar('a' + i);
+}
+	_putchar('\n');
+}
