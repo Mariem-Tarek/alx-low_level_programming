2dcacbf1203ce8af666f7ae67b65bbddf4754393
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-12 15:27:38 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _abs  - function prints
+ *
+ * @x: parameter that taken to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int x)
+{
+if (x >= 0)
+{
+	x = x;
+}
+else
+{
+	x = x * (-1);
+}
+	return (x);
+}
