260fea1fc3756bd096319606a3e0d681d2682ed8
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-13 14:34:18 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function prints
+ *
+ * @a : parameter to be checked
+ * @b : parameter to be cheaked
+ *
+ * Return: 0 always (Successd)
+ */
+int add(int a, int b)
+{
+int result;
+result = a + b;
+printf("%d", result);
+return (0);
+}
