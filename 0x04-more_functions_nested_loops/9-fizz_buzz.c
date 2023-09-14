a64a398ca40ae0911556b0191824693a01ec003a
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-14 16:01:08 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Description: A C programme
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("%s", "FizzBuzz");
+putchar(' ');
+}
+else if (i % 3 == 0 && i % 5 != 0)
+{
+printf("%s", "Fizz");
+putchar(' ');
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("%s", "Buzz");
+putchar(' ');
+}
+else
+{
+printf("%d", i);
+putchar(' ');
+}
+}
+return (0);
+}
+
