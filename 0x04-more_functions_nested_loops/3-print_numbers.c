1b5698de3b4a4d99c49736ae086bcc5cacf469c1
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-14 12:43:34 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints
+ *
+ * Return: no return
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+putchar (i + '0');
+
+putchar('\n');
+}
