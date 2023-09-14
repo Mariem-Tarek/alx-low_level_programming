abbffafd3805d38edbe417c2cc4cb6c76b44637b
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-14 12:19:23 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - function prints
+ *
+ * @c: parameters that taken to be checked
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
