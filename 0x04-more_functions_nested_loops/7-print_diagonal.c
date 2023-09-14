c80cf5d96cb1500493202a0f0d8179525e98bb51
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-14 14:05:23 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,20 @@
  */
 void print_diagonal(int n)
 {
+int i;
+int j;
 if (n <= 0)
 {
 _putchar('\n');
 }
 else
 {
-int i;
 for (i = 0; i < n; i++)
 {
-int j;
 for (j = 0; j < i; j++)
 putchar (' ');
 
-_putchar(92);
+_putchar(92);/*is equal to '/' char*/
 _putchar('\n');
 }
 }
