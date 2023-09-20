9d76e4d33a0b30dacf1cd21bc62f7b6c9ca2bca0
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-19 17:30:25 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int - a new function
+*
+* @a : int prametar
+* @b : int prametar
+*
+* Return: Nothing
+*/
+void swap_int(int *a, int *b)
+{
+int three;
+three = *a;
+*a = *b;
+*b = three;
+}
