a0dcf8e8e439cadd6f10a72aa46663799454724f
Mariem Tarek <mariemtarek283@gmail.com>
2023-09-21 16:35:55 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* cap_string - a new function
+*
+* @n : int prametar
+*
+* Return: original
+*/
+char *cap_string(char *n)
+{
+char *original = n;
+while (*n != '\0')
+{
+if (*n >= 'a' && *n <= 'z')
+{
+*n = *n - 32;
+}
+n++;
+}
+return (original);
+}
