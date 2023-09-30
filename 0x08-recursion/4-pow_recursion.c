71eb9768451bb629996fb86b33963ab7e9b3f01c
Mariem <mariemtarek283@gmail.com>
2023-09-30 04:12:52 +0300

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _pow_recursion - a new function
+*
+* @x : int prametar
+* @y : int prametar
+*
+* Return: nothing or _pow_recursion
+*/
+int _pow_recursion(int x, int y)
+{
+if (y == 0){
+return (1);
+}
+else if(y < 0)
+return (-1);
+
+y--;
+return (x * _pow_recursion(x , y));
+}
