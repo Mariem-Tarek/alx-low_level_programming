f9aee3f089f7829eb0ca610dc0174c7e9bb6a058
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-05 15:55:56 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ * @argc : int
+ * @argv : string
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
+
