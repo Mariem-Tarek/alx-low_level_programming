93967997734fe76660bc45c9e137b16fdef2484c
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-05 15:43:21 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * @argc : int prametar
+ * @argv : string prametar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+printf("%s \n", argv[0]);
+(void)argc;
+return (0);
+}
