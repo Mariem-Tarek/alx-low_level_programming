9e6ac291565013b2d91bd5e436dbd731e345c0f2
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-05 16:01:49 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ * @argc : int
+ * @argv : string
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+int i;
+
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+
+return (0);
+}
+
