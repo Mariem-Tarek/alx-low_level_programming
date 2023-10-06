e92a752802fb1164a8a493b28573063ee73141ec
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-05 17:24:48 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,30 +6,38 @@
  * Description: A C program that prints with put function
  * @argc : int
  * @argv : string
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
 {
 int sum;
 int i;
+int count;
+count = 0;
 
-if (argc != 1)
+if (argc == 1)
 {
 printf("%d\n", 0);
 }
 else
 {
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
-
-if (*argv[i] >= 48 && *argv[i] >= 57)
+if (*argv[i] >= '0' && *argv[i] <= '9')
 {
 sum += atoi(argv[i]);
 }
 else
 {
 printf("Error");
+count++;
+break;
 }
+}
+if (count == 0)
+{
+printf("%d\n", sum);
 }
 }
 return (0);
