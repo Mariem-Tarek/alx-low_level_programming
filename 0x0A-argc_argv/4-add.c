19be0821b180047de44395fc9b0b3de71702ec15
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-05 18:06:40 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,16 @@
  */
 int main(int argc, char *argv[])
 {
-int sum = 0;
-char *c;
+	int sum = 0;
+	char *c;
 
-while (--argc)
-{
-for (c = argv[argc]; *c; c++)
-{
-
-if (*c < '0' || *c > '9')
-return (printf("Error\n"), 1);
-
-sum += atoi(argv[argc]);
+	while (--argc)
+	{
+	for (c = argv[argc]; *c; c++)
+		if (*c < '0' || *c > '9')
+			return (printf("Error\n"), 1);
+	sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
-}
-printf("%d\n", sum);
-return (0);
-}
-
