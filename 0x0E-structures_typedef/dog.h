7fe970ac9e57d8f24ee74549ed69d9a723474424
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-10 17:39:21 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a new struct with c language
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
