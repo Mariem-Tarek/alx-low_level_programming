cfa74cdd7f6b4c1efb0f107fb57f7bad8d73d120
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-20 04:08:04 +0300

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _strlen - c function
+ *
+ * @s: string
+ *
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+	return (0);
+
+	while (*s++)
+	i++;
+	return (i);
+}
+
+/**
+ * print_list - c function
+ *
+ * @h: string - (malloc'ed string)
+ *
+ * Return: i
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0
+		;
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+		printf("[%ld] (nil)\n", i);
+		else
+		printf("[%ld] %s\n", i, h->str);
+
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
