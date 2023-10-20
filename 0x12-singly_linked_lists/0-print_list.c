ef2359f8dd628b7e40d01ae53af698a5209d9a2b
Mariem Tarek <mariemtarek283@gmail.com>
2023-10-20 04:40:12 +0300

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * _strlen - c function
- *
- * @s: string
- *
- * Return: i
- */
-
+* _strlen - a new function
+*
+* @s : int prametar
+*
+* Return: i
+*/
 int _strlen(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	if (!s)
-	return (0);
+if (!s)
+return (0);
 
-	while (*s++)
-	i++;
-	return (i);
+while (*s++)
+i++;
+return (i);
 }
-
 /**
- * print_list - c function
- *
- * @h: string - (malloc'ed string)
- *
- * Return: i
- */
-
+* print_list - a new function
+*
+* @h : int prametar
+*
+* Return: i
+*/
 size_t print_list(const list_t *h)
 {
-	size_t i = 0
-		;
-	while (h != NULL)
-	{
-	if (h->str == NULL)
-		printf("[%ld] (nil)\n", _strlen(h->str));
-		else
-		printf("[%ld] %s\n", _strlen(h->str), h->str);
+size_t i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] (nil)\n", _strlen(h->str));
+else
+printf("[%d] %s\n", _strlen(h->str), h->str);
 
-		h = h->next;
-		i++;
-	}
-	return (i);
+h = h->next;
+i++;
+}
+return (i);
 }
